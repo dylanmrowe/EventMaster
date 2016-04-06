888d52f0fb72ea001fe45328f8076c34ab52cfa2
Andrew Wheeler <awheeler294@gmail.com>
2016-04-05 22:24:58 -0600

    commit

--- js/events.js
+++ js/events.js
@@ -61,27 +61,45 @@ function renderEvent(event, eventContainer) {
     var description = event.eventDescription;
     var eventDate = moment(event.eventDate + ' ' + event.eventTime);
 
-    var action = '';
-    if (eventDate.isBefore(Date.now())) {
-        action = '<button class="smallButton">Rate Event</button>';
-    }
-    else if (event.trashed) {
-        action = '<button class="smallButton">Remove</button>';
-    }
-    else {
-        action = '<label><input type="checkbox" name="attendingStatus" value="going" class="checkbox">Going</label>';
-    }
+    //var action = '';
+    //if (eventDate.isBefore(Date.now())) {
+    //    action = '<button class="smallButton">Rate Event</button>';
+    //}
+    //else if (event.trashed) {
+    //    action = '<button class="smallButton">Remove</button>';
+    //}
+    //else {
+    //    action = '<label><input type="checkbox" name="attendingStatus" value="going" class="checkbox">Going</label>';
+    //}
 
     eventContainer.append("" +
         '<div class="eventContainer">' +
-        '<a href="#" class="eventLink">' +
-        '<div class="event">' +
-        '<img src="' + img + '" class="eventPhoto">' +
-        '<div class="eventDetails">' +
-        '<p class="eventDescription">' + description + '</p>' +
-        '<p class="dateAndTime"> ' + eventDate.format('MMMM Do [@] h:mm a') + '<br> &nbsp </p>' +
-        '</div>' +
-        '</div>' +
-        '</a>' +
+        '   <a href="#" class="eventLink">' +
+        '      <div class="event">' +
+        '          <img src="' + img + '" class="eventPhoto">' +
+        '          <div class="eventDetails">' +
+        '          <p class="eventDescription">' + description + '</p>' +
+        '          <p class="dateAndTime">' + eventDate.format('MMMM Do [@] h:mm a') + '<br> &nbsp </p>' +
+        '      </div>' +
+        '   </a>' +
+        '</div>');
+}
+
+function renderEventLarge(event, eventContainer) {
+
+    var img = event.eventImg;
+    var description = event.eventDescription;
+    var eventDate = moment(event.eventDate + ' ' + event.eventTime);
+
+    eventContainer.append("" +
+        '<div class="large-event-container">' +
+        '   <a href="#" class="eventLink">' +
+        '       <div class="event">' +
+        '           <img src="' + img + '" class="eventPhoto">' +
+        '           <p class="eventDescription">' + description + '</p>' +
+        '           <p class="dateAndTime">' + eventDate.format('ddd, MMM h:mm a') + '<br> &nbsp </p>' +
+        '           <div class="eventDetails">' +
+        '       </div>' +
+        '   </a>' +
         '</div>');
 }
